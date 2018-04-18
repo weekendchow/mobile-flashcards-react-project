9e6042bc001bfb0b702f860922d913a13775fbba
mozhou <mozhou216@gmail.com>
2018-04-18 12:07:01 -0400

    finish_button_update

--- components/Quiz.js
+++ components/Quiz.js
@@ -17,6 +17,24 @@ class Quiz extends Component {
     showFinish: false
   }
 
+  restartQuiz = () => {
+    this.props.navigation.navigate(
+      'Quiz', {
+        questions: this.props.deck.questions,
+        deck: this.props.deck.title
+      }
+    )
+  }
+
+  backToDeck = () => {
+    this.props.navigation.navigate(
+      'DeckDetail', {
+        questions: this.props.deck.questions,
+        title: this.props.deck.title
+      }
+    )
+  }
+
   render () {
     const { questions, title } = this.props.deck
     const { currentQuestion, correct, showAnswer, showFinish } = this.state
@@ -24,8 +42,8 @@ class Quiz extends Component {
 
     if(!showFinish) {
       return (
-        <View style={styles.container}>
-          <Text style={{color: purple}}>
+        <View style={styles.containerQuiz}>
+          <Text style={{color: purple, alignSelf:'flex-start'}}>
             {currentQuestion +'/'+ questions.length}
           </Text>
           <Text style={styles.question}>
@@ -84,17 +102,32 @@ class Quiz extends Component {
       )
     }else {
       return (
-        <View style={styles.finish}>
-          <Text style={{fontSize:30, fontWeight:'bold', color: purple, margin:10}}>
-            Congratulation!
-          </Text>
-          <Text style={{fontSize:25, color: purple, margin:5}}>
-            You have complete the Quiz!
-          </Text>
-          <Text style={{fontSize:20, color: orange, margin:20}}>
-            accuracy: {accuracy.toFixed(2)}%
-          </Text>
+        <View style={styles.containerFinish}>
+          <View style={styles.contentContainer}>
+            <Text style={{fontSize:30, fontWeight:'bold', color: purple, margin:10}}>
+              Congratulation!
+            </Text>
+            <Text style={{fontSize:25, color: purple, margin:5}}>
+              You have complete the Quiz!
+            </Text>
+            <Text style={{fontSize:20, color: orange, margin:20}}>
+              accuracy: {accuracy.toFixed(2)}%
+            </Text>
+          </View>
+          <View style={styles.finish}>
+            <TouchableOpacity
+              style={styles.restartQuizBtn}
+              onPress={this.restartQuiz}>
+              <Text style={styles.restartQuizTxt}>Restart Quiz</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.backToDeckBtn}
+              onPress={this.backToDeck}>
+              <Text style={styles.backToDeckTxt}>Back to Deck</Text>
+            </TouchableOpacity>
+          </View>
         </View>
+
       )
     }
   }
@@ -107,50 +140,91 @@ function mapStateToProps(state, { navigation }) {
 }
 
 const styles=StyleSheet.create({
-    container:{
-      flex:1,
-    },
-    question:{
-      fontSize:30,
-      fontWeight: 'bold',
-      padding:20,
-      marginTop:150,
-      color: purple,
-      textAlign:'center'
-    },
-    center: {
-      textAlign: 'center',
-      fontSize: 20,
-      color: orange,
-    },
-    correctBtn: {
-      backgroundColor: green,
-      padding: 10,
-      borderRadius: 7,
-      height: 45,
-      marginLeft: 80,
-      marginRight: 80,
-    },
-    incorrectBtn: {
-      backgroundColor: red,
-      padding: 10,
-      borderRadius: 7,
-      height: 45,
-      marginLeft: 80,
-      marginRight: 80,
-      marginTop: 20,
-      marginBottom: 60,
-    },
-    submitBtnText: {
-      color: white,
-      fontSize: 22,
-      textAlign: 'center'
-    },
-    finish: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }
+  containerQuiz: {
+    flex: 1,
+  },
+  containerFinish:{
+    flex: 1,
+    backgroundColor: white,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: white,
+    borderTopWidth: 40,
+    borderBottomWidth: 40,
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  question:{
+    fontSize:30,
+    fontWeight: 'bold',
+    padding:20,
+    marginTop:150,
+    color: purple,
+    textAlign:'center'
+  },
+  center: {
+    textAlign: 'center',
+    fontSize: 20,
+    color: orange,
+  },
+  correctBtn: {
+    backgroundColor: green,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 80,
+    marginRight: 80,
+  },
+  incorrectBtn: {
+    backgroundColor: red,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 80,
+    marginRight: 80,
+    marginTop: 20,
+    marginBottom: 60,
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: 'center'
+  },
+  restartQuizBtn: {
+    padding: 13,
+    margin: 5,
+    width: 200,
+    backgroundColor: white,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: purple,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  restartQuizTxt: {
+    color: purple,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  backToDeckBtn: {
+    padding: 13,
+    margin: 5,
+    width: 200,
+    backgroundColor: purple,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: purple,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  backToDeckTxt: {
+    fontWeight: 'bold',
+    color: white,
+    textAlign: 'center'
+  }
 
 })
 
